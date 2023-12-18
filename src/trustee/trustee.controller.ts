7a45767c6c5250dcb43f5fc4eeea28f474a2b2ee
priyanshuy-edviron <priyanshu.yogi@edviron.com>
2023-12-18 17:53:59 +0530

    create-school completed

--- src/trustee/trustee.controller.ts
+++ src/trustee/trustee.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Get, Body, BadRequestException, ConflictException, Param, UseGuards, Request } from '@nestjs/common';
+import { TrusteeService } from './trustee.service';
 
 @Controller('trustee')
-export class TrusteeController {}
+export class TrusteeController {
+    constructor (private trusteeService:TrusteeService){}
+
+    @Post('create-school')
+    async createSchool(
+        @Body()
+        body:{name: string; phone_number: string, email: string, school_name: string},
+
+        @Request() req
+    ){
+
+        if(!body.name || !body.phone_number || !body.email || !body.school_name)
+        {
+            throw new BadRequestException('Fill all fields')
+        }
+
+        try{
+            
+            const school = await this.trusteeService.createSchool(body.phone_number, body.name, body.email, body.school_name, req.user);
+            return school;
+        }catch(error){
+            console.log(error.response);
+            if(error.response.statusCode === 409){
+                throw new ConflictException(error.message)
+            }
+            throw new BadRequestException(error.message)
+        }
+    }
+    
+}
--- src/trustee/trustee.service.ts
+++ src/trustee/trustee.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException, NotFoundException, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Trustee } from './schema/trustee.schema';
+import * as  mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import axios from 'axios';
+import { TrusteeSchool } from './schema/school.schema';
+
+
 
 @Injectable()
-export class TrusteeService {}
+export class TrusteeService {
+    constructor(
+        @InjectModel(Trustee.name)
+        private trusteeModel: mongoose.Model<Trustee>,
+        @InjectModel(TrusteeSchool.name)
+        private trusteeSchoolModel:mongoose.Model<TrusteeSchool>,
+        private readonly jwtService: JwtService
+    ) { }
+
+      async createSchool(phone_number: string, name: string, email: string, school_name: string, trustee: string){
+
+        try{   
+            const data = {
+              phone_number, name, email, school_name
+            }
+            const token = this.jwtService.sign(data,{secret:process.env.PRIVATE_TRUSTEE_KEY});
+            
+            const school = await axios.post(`${process.env.MAIN_SERVER_URL}/create-school`,{
+                token:token
+            })
+
+            const trusteeSchool = await this.trusteeSchoolModel.create({school_id: school.data.adminInfo.school_id, school_name:school.data.updatedSchool.updates.name, trustee_id: trustee})
+            
+            return school.data
+        }catch (error) {
+          
+          if (error.response) {
+            // The request was made and the server responded with a non-success status code
+            if (error.response.status === 409) {
+              throw new ConflictException(error.response.data.message);
+            } else if (error.response.data.message == 'Invalid phone number!'){
+              throw new BadRequestException('Invalid phone number!')
+            } else if (error.response.data.message == 'Invalid email!'){
+              throw new BadRequestException('Invalid email!')
+            } else if(error.response.data.message ==='User already exists'){
+              throw new BadRequestException('User already exists');
+            } else {
+              // Handle other server response errors
+              throw new BadRequestException('Failed to create school');
+            }
+          } else if (error.request) {
+            // The request was made but no response was received
+            throw new BadRequestException('No response received from the server');
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log(error);
+            
+            throw new BadRequestException('Request setup error');
+          }
+        }
+    }
+    
+
+}
