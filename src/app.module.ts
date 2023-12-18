3b81324b12ec1c56bbf04bab6838b8454a9b5d26
Jasraj Singh Bhatia <singhbhatiajasraj@gmail.com>
2023-12-18 13:56:59 +0530

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { ApiService } from './api/api.service';
-import { ApiKeyModule } from './api/api_key/api_key.module';
-import { SectionModule } from './api/section/section.module';
-import { StudentModule } from './api/student/student.module';
-import { SchoolModule } from './api/school/school.module';
+// import { ApiKeyModule } from './api/api_key/api_key.module';
+// import { SectionModule } from './api/section/section.module';
+// import { StudentModule } from './api/student/student.module';
+// import { SchoolModule } from './api/school/school.module';
 
 @Module({
-  imports: [ApiKeyModule, SectionModule, StudentModule, SchoolModule],
+  imports: [
+    // ApiKeyModule, SectionModule, StudentModule, SchoolModule
+  ],
   controllers: [AppController, ApiController],
   providers: [AppService, ApiService],
 })
