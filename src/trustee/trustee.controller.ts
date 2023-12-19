77f4a4c0d91c7d291b387e0f93847bd85217687b
prashantv-edviron <prashant.verma@edviron.com>
2023-12-19 18:29:34 +0530

    merge with dev

--- src/trustee/trustee.controller.ts
+++ src/trustee/trustee.controller.ts
@@ -1,43 +1,42 @@
-import { Controller,Post,Get, Body, BadRequestException, ConflictException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  BadRequestException,
+  ConflictException,
+} from '@nestjs/common';
 import { TrusteeService } from './trustee.service';
 import { Trustee } from './schema/trustee.schema';
-import * as jwt from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken';
 import { JwtPayload } from 'jsonwebtoken';
-
-
 
 @Controller('trustee')
 export class TrusteeController {
-    constructor (
-        private trusteeService:TrusteeService
-        ){}
+  constructor(private trusteeService: TrusteeService) {}
 
-    @Get()
-    async findTrustee():Promise<Trustee[]>{
-        return this.trusteeService.findTrustee()
-    }
- 
+  @Get()
+  async findTrustee(): Promise<Trustee[]> {
+    return this.trusteeService.findTrustee();
+  }
 
-    @Post()
-    async createTrustee(
-        @Body()
-        token
-    ):Promise<Trustee>{
-        try{
-            
-            
-            
-            const info: JwtPayload = jwt.verify(token.data,process.env.PRIVATE_TRUSTEE_KEY) as JwtPayload;
-            const credential = await this.trusteeService.createTrustee(info);
-            return credential
-             
-        }catch(e){
-            
-            if(e.response.statusCode === 409){
-                throw new ConflictException(e.message)
-            }
-            throw new BadRequestException(e.message)  
-        }
+  @Post()
+  async createTrustee(
+    @Body()
+    token,
+  ): Promise<Trustee> {
+    try {
+      const info: JwtPayload = jwt.verify(
+        token.data,
+        process.env.PRIVATE_TRUSTEE_KEY,
+      ) as JwtPayload;
+      const credential = await this.trusteeService.createTrustee(info);
+      return credential;
+    } catch (e) {
+      if (e.response.statusCode === 409) {
+        throw new ConflictException(e.message);
+      }
+      throw new BadRequestException(e.message);
     }
-    
+  }
 }
--- src/trustee/trustee.service.ts
+++ src/trustee/trustee.service.ts
@@ -1,55 +1,82 @@
-import { ConflictException, Injectable, UnauthorizedException, NotFoundException, BadRequestException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Trustee } from './schema/trustee.schema';
-import * as  mongoose from 'mongoose';
-
-
+import * as mongoose from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class TrusteeService {
-    constructor(
-        @InjectModel(Trustee.name)
-        private trusteeModel: mongoose.Model<Trustee>
-    ) { }
+  constructor(
+    @InjectModel(Trustee.name)
+    private trusteeModel: mongoose.Model<Trustee>,
+    private jwtService: JwtService,
+  ) {}
 
+  async findTrustee(): Promise<Trustee[]> {
+    const trustee = await this.trusteeModel.find();
+    return trustee;
+  }
 
-    async findTrustee(): Promise<Trustee[]> {
-        const trustee = await this.trusteeModel.find()
-        return trustee
+  async createTrustee(info): Promise<Trustee> {
+    const { name, email, password, school_limit } = info;
+    try {
+      const checkMail = await this.trusteeModel
+        .findOne({ email_id: email })
+        .exec();
+
+      if (checkMail) {
+        throw new ConflictException(`${email} already exist`);
+      }
+
+      const trustee = await new this.trusteeModel({
+        name: name,
+        email_id: email,
+        password_hash: password,
+        school_limit: school_limit,
+      }).save();
+      return trustee;
+    } catch (error) {
+      if (error.response.statusCode === 409) {
+        throw new ConflictException(error.message);
+      }
+      throw new BadRequestException(error.message);
     }
+  }
+  async createApiKey(trusteeId: string): Promise<string> {
+    try {
+      if (!Types.ObjectId.isValid(trusteeId)) {
+        throw new BadRequestException('Invalid trusteeId input');
+      }
+      const trustee = await this.trusteeModel.findById(trusteeId, {
+        password_hash: 0,
+      });
 
-    
-    async createTrustee(info):Promise<Trustee> {
+      if (!trustee) {
+        throw new NotFoundException('Trustee not found');
+      }
 
-        
+      trustee.IndexOfApiKey++;
+      const updatedTrustee = await trustee.save();
+      const payload = {
+        trusteeId: updatedTrustee._id,
+        IdexOfApiKey: updatedTrustee.IndexOfApiKey,
+      };
+      const apiKey = this.jwtService.sign(payload, {
+        secret: process.env.API_JWT_SECRET,
+      });
 
-        const { name, email, password, school_limit } = info
-        try {
-            const checkMail = await this.trusteeModel.findOne({ email_id: email }).exec()
-
-            if (checkMail) {
-                
-                throw new ConflictException(`${email} already exist`)
-            }
-
-            const trustee = await new this.trusteeModel({
-                name: name,
-                email_id: email,
-                password_hash: password,
-                school_limit: school_limit,
-            }).save();
-            return trustee
-
-        } catch (error) {
-            
-            if(error.response.statusCode === 409){
-                 
-                throw new ConflictException(error.message)
-            }
-            throw new BadRequestException(error.message)
-        }
+      return apiKey;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      }
+      throw new BadRequestException(error.message);
     }
-
-
-
+  }
 }
