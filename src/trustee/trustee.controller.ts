834425e46a9bf0f600df0ba2e2a8f39b830ab8da
priyanshuy-edviron <priyanshu.yogi@edviron.com>
2023-12-19 17:31:17 +0530

    added validate api service and controller

--- src/trustee/trustee.controller.ts
+++ src/trustee/trustee.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { NotFoundException, Param, Get, Controller, Post, Body, BadRequestException, UnauthorizedException, Req } from '@nestjs/common';
+import { TrusteeService } from './trustee.service';
+import * as JWT from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken'
+import {JwtService} from '@nestjs/jwt'
+
+
 
 @Controller('trustee')
-export class TrusteeController {}
+export class TrusteeController {
+    constructor (
+        private trusteeService:TrusteeService,
+        private readonly jwtService: JwtService
+        ){}
+
+    @Get('validate')
+    async validateApiKey(
+      @Req() req,
+    ):Promise<{payload: any}> {
+
+      try {
+        const authorizationHeader = req.headers.authorization;
+
+        if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+        throw new UnauthorizedException('Invalid Authorization header format');
+        }
+
+        const token = authorizationHeader.split(' ')[1];
+
+        const trustee = await this.trusteeService.validateApiKey(token);
+      return trustee;
+      } catch (error) {
+        if(error instanceof NotFoundException){
+          throw new NotFoundException(error.message)
+        } else {
+          throw new UnauthorizedException(error.message);
+        }
+      }
+    }  
+}
--- src/trustee/trustee.service.ts
+++ src/trustee/trustee.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+  } from '@nestjs/common';
+  import { InjectModel } from '@nestjs/mongoose';
+  import mongoose, { Types } from 'mongoose';
+  import { Trustee } from './schema/trustee.schema';
+  import { JwtService } from '@nestjs/jwt';
+  
+  @Injectable()
+  export class TrusteeService {
+    constructor(
+      @InjectModel(Trustee.name)
+      private trusteeModel: mongoose.Model<Trustee>,
+      private readonly jwtService: JwtService,
+    ) {}
 
-@Injectable()
-export class TrusteeService {}
+
+    async validateApiKey(apiKey: string): Promise<any> {
+        try {
+          const decodedPayload = this.jwtService.verify(apiKey, {
+            secret: process.env.JWT_FOR_TRUSTEE_AUTH,
+          });
+
+          const trustee = await this.trusteeModel.findById(decodedPayload.trusteeId);
+
+          if(!trustee)
+            throw new NotFoundException('trustee not found')
+    
+          if(trustee.IndexOfApiKey !== decodedPayload.IndexOfApiKey)
+            throw new Error('API key expired')
+          
+        const userTrustee = {id: trustee._id, name: trustee.name, email: trustee.email_id}
+
+          return userTrustee;
+        } catch (error) {
+            // console.log(error);
+            
+          throw new UnauthorizedException("Invalid API key");
+        }
+      }
+  }
