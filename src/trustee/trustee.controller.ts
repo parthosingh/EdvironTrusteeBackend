822557297b1ea4f8a3d7e1be0c2b9751c4dff748
rajb-edviron <raj.barmaiya@edviron.com>
2023-12-19 09:59:10 +0530

    create section by trustee

--- src/trustee/trustee.controller.ts
+++ src/trustee/trustee.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Get, Body, BadRequestException, ConflictException, Query } from '@nestjs/common';
+import { TrusteeService } from './trustee.service';
 
 @Controller('trustee')
-export class TrusteeController {}
+export class TrusteeController {
+    constructor (private trusteeService:TrusteeService){}
+
+ 
+@Post('section')
+    async createSection(
+        @Body()
+        body:{school_id:string;data:{className:string,section:string}}
+    ){
+       
+        try{
+            
+            const section = await this.trusteeService.createSection(body.school_id,body.data)
+            return section
+        }catch(error){ 
+            
+            if(error.response.statusCode === 409){
+                throw new ConflictException(error.message)
+            }
+            throw new BadRequestException(error.message)
+        }
+    }
+
+    
+}
--- src/trustee/trustee.service.ts
+++ src/trustee/trustee.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException, NotFoundException, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Trustee } from './schema/trustee.schema';
+import * as  mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import axios from 'axios';
+import { Types, ObjectId } from 'mongoose';
+const url = process.env.MAIN_SERVER_URL
+import {JwtPayload} from 'jsonwebtoken'
 
 @Injectable()
-export class TrusteeService {}
+export class TrusteeService {
+    constructor(
+        @InjectModel(Trustee.name)
+        private trusteeModel: mongoose.Model<Trustee>
+    ) { }
+
+
+ async createSection(school_id,data:object){
+
+        try{
+            
+            if (!Types.ObjectId.isValid(school_id)) {
+                throw new BadRequestException('Invalid school_id format');
+            }
+            
+            const info = {
+                school_id:school_id,
+                data:data 
+            }
+            const token = jwt.sign(info,process.env.PRIVATE_TRUSTEE_KEY,{expiresIn:'1h'})
+            
+            const section = await axios.post(`${process.env.MAIN_SERVER_URL}/api/trustee/section`,{
+                token:token
+            }) 
+            return section.data
+        }catch(error){
+            if(error.response.data.statusCode === 409){
+                throw new ConflictException(error.response.data.message)
+            }
+            throw new BadRequestException(error.message);
+        }
+    }
+    
+    
+}
