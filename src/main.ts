f00c64d4d8411b00ceceb573e7b455f0c397f660
priyanshuy-edviron <priyanshu.yogi@edviron.com>
2023-12-29 11:19:19 +0530

    added cors config in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const whitelist = [
