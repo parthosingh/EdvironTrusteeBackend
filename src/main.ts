9557e6aa7b1a75101981518063d921e54671f190
Jasraj Singh Bhatia <singhbhatiajasraj@gmail.com>
2024-02-16 19:52:08 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,8 @@ async function bootstrap() {
     'https://dev.onboarding.edviron.com',
     'https://qa.onboarding.edviron.com',
     'https://dev.trustee.edviron.com',
-    'https://dev.api.edviron.com'
+    'https://dev.api.edviron.com',
+    "https://trustee.edviron.com"
   ];
   app.enableCors({
     origin: function (origin, callback) {
