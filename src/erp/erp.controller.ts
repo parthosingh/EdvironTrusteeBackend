b662ca761e225510b274097ff3e0e254c5211272
raj0811 <rpbarmaiya@gmail.com>
2025-06-10 16:20:05 +0530

    test

--- src/erp/erp.controller.ts
+++ src/erp/erp.controller.ts
@@ -989,9 +989,9 @@ export class ErpController {
         } else {
           for (const vendor of vendors_info) {
             // Check if vendor_id is present
-            if (!vendor.vendor_id) {
-              throw new BadRequestException('Vendor ID is required');
-            }
+            // if (!vendor.vendor_id) {
+            //   throw new BadRequestException('Vendor ID is required');
+            // }
 
             const vendors_data = await this.trusteeService.getVenodrInfo(
               vendor.vendor_id,
